ec0ee47f2f3f459485e59ec1cbad0adc1d8a6e1e
kjk <kjk@4science.net>
2022-12-12 13:30:22 +0900

    22121201

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Facades\URL;
+// use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider {
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider {
     
     public function boot() {
         
-        if(head( explode('://', config('app.url'))) == 'https') { URL::forceScheme('https'); }
+        // if(head( explode('://', config('app.url'))) == 'https') { URL::forceScheme('https'); }
         //  다형성 타입 사용자 정의
         //  한 모델의 여러 모델이 관계형성을 할때 키값과 타입값이 같이 들어간다
         //  타입값으로 모델의 경로 + 모델명이 들어가는데 이렇게 제정의 한다.
