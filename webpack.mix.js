7ef0aa0e3360973cb4e4e579c3cd68fa2ae0cc62
kjk <kjk@4science.net>
2022-12-22 14:09:15 +0900

    22122201

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,7 +20,7 @@ mix.js('resources/js/app.js', 'public/js')
         },
     });
 
-mix.browserSync ( 'https://dev.4science.net/');
+mix.browserSync ( 'https://real.4science.net/');
 // mix.browserSync (process.env.APP_URL);
 // mix.disableNotifications();
 // mix.browserSync({
