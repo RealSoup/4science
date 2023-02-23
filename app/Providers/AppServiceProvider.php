f0740b7b59e3317120478f8350038f472c971dff
kjk <kjk@4science.net>
2023-02-23 13:28:53 +0900

    23022304

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,9 @@ class AppServiceProvider extends ServiceProvider {
     }
 
     public function boot() {
+        if (env('APP_ENV') == 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         // if(config('app.env') === 'production') {
         //     \URL::forceScheme('https');
         // }
