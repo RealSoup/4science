a739f39cd465e2784cb16c14c2270f2f292ac870
kjk <kjk@4science.net>
2023-03-31 21:23:23 +0900

    23033104

--- public/index.php
+++ public/index.php
@@ -1,11 +1,10 @@
 <?php
 
-if($_SERVER["REMOTE_ADDR"] !== '10.0.13.119')
+if($_SERVER["REMOTE_ADDR"] !== '125.132.234.16')
     echo "<div style='text-align:center;'><img src='https://fourscience.s3.ap-northeast-2.amazonaws.com/common/no_access.jpg' width='80%' /></div>";
-else
-    echo 123;
 
-    var_dump($_SERVER["REMOTE_ADDR"]);
+
+    var_dump($_SERVER['HTTP_X_FORWARDED_FOR']);
 return false;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
