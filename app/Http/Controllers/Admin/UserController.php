8290620181402eb70f57beab1132842b2931cc2f
kjk <kjk@4science.net>
2023-06-30 10:08:23 +0900

    23063002

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -162,31 +162,31 @@ class UserController extends Controller {
 			$temp = explode(";", $req->temp);
 			foreach($temp as $k => $v)
 				$list[] =  collect(['name' => 'A'.$k, 'email' => $v]);
-            self::postman(collect($list));
+            self::postman($req, collect($list));
 		} else {
 			$count = User::when($req->target == 1, fn ($q, $v) => $q->where('receive_mail', 'Y'))
             ->whereIn('id', [130, 131, 39543, 70001, 70002, 70005, 70007, 70009, 70010, 70011, 70012, 70013, 70014, 70015])
             ->count();
             // 한번에 보낼수 있는 최고 양이 3만통
             $list = User::select('name', 'email')
-                        ->member()
+                        // ->member()
                         ->when($req->target == 1, fn ($q, $v) => $q->where('receive_mail', 'Y'));
 
-            $list->whereIn('id', [130, 131, 39543, 70001, 70002, 70005, 70007, 70009, 70010, 70011, 70012, 70013, 70014, 70015]);
+            $list->whereIn('id', [39, 130, 131, 39543, 70001, 70002, 70005, 70007, 70009, 70010, 70011, 70012, 70013, 70014, 70015]);
 
-            $limit = 5;
-            if(intval($count) > $limit) {
-                $i=0;
-                while ($i < $count) {
-                    $list->offset($i)->limit($limit);
-                    self::postman($list->get());
-                    $i+=$limit;
-                }
+            $limit = 30000;
+            
+            $i=0;
+            while ($i < $count) {
+                $list->offset($i)->limit($limit);
+                self::postman($req, $list->get());
+                $i+=$limit;
             }
+            
 		}
     }
 
-    public function postman($list) {
+    public function postman($req, $list) {
         
 		/******************** 인증정보 ********************/
 		// 대량메일 인증 관련
@@ -207,9 +207,9 @@ class UserController extends Controller {
         }
 
 		// 파일첨부 관련
-		$file_name = $_FILES['addfile']['name'];
-		$tmp_name = $_FILES['addfile']['tmp_name'];
-		$content_type = $_FILES['addfile']['type'];
+		// $file_name = $req['file']?$req['file']['name']:null;
+		// $tmp_name = $req['file']?$req['file']['tmp_name']:null;
+		// $content_type = $req['file']?$req['file']['type']:null;
 
 		/******************** 요청변수 처리 ********************/
 		$mail['secureKey'] = $secureKey;
@@ -223,7 +223,7 @@ class UserController extends Controller {
 
 		$host_info = explode("/", $sendmail_url);
 		$host = $host_info[2];
-		$path = $host_info[3]."/".$host_info[4];
+		$path = $host_info[3];
 
 		srand((double)microtime()*1000000);
 		$boundary = "---------------------".substr(md5(rand(0,32000)),0,10);
@@ -234,6 +234,7 @@ class UserController extends Controller {
 		$header .= "Content-type: multipart/form-data, boundary=".$boundary."\r\n";
 
 		// 본문 생성
+        $data='';
 		foreach($mail AS $index => $value){
 			$data .="--$boundary\r\n";
 			$data .= "Content-Disposition: form-data; name=\"".$index."\"\r\n";
@@ -242,14 +243,14 @@ class UserController extends Controller {
 		}
 
 		// 첨부파일
-		if (is_uploaded_file($_FILES['addfile']['tmp_name'])) {
-			$data .= "--$boundary\r\n";
-			$content_file = join("", file($tmp_name));
-			$data .="Content-Disposition: form-data; name=\"addfile\"; filename=\"".$file_name."\"\r\n";
-			$data .= "Content-Type: $content_type\r\n\r\n";
-			$data .= "".$content_file."\r\n";
-			$data .="--$boundary--\r\n";
-		}
+		// if (is_uploaded_file($req['file']['tmp_name'])) {
+		// 	$data .= "--$boundary\r\n";
+		// 	$content_file = join("", file($tmp_name));
+		// 	$data .="Content-Disposition: form-data; name=\"addfile\"; filename=\"".$file_name."\"\r\n";
+		// 	$data .= "Content-Type: $content_type\r\n\r\n";
+		// 	$data .= "".$content_file."\r\n";
+		// 	$data .="--$boundary--\r\n";
+		// }
 		$header .= "Content-length: " . strlen($data) . "\r\n\r\n";
 
 		$fp = fsockopen($host, 80);
@@ -267,12 +268,7 @@ class UserController extends Controller {
 
 			$msg = explode("\r\n\r\n",trim($rsp));
 			echo $msg[1];
-		}
-		else {
-			echo "Connection Failed";
-		}
-		if ( $msg[1] == "SUCCESS" ) {
-			$this->tinyjs->pageRedirect($this->input->server("HTTP_REFERER"), '메일 발송 요청 완료');
-		}
+		} else echo "Connection Failed";
+		if ( $msg[1] == "SUCCESS" ) return response()->json('success', 200);
 	}
 }
