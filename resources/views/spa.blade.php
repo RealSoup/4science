a933faf9c0c19ce7880f1df1ad21ecc4ce1714c1
kjk <kjk@4science.net>
2023-07-05 08:58:42 +0900

    23070503

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -28,11 +28,11 @@
     <!-- 구글링 결과로 정확한 이유는 모른다. -->
 
     <!-- Google Tag Manager -->
-    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NJK884V');</script> -->
+    })(window,document,'script','dataLayer','GTM-NJK884V');</script>
     <!-- End Google Tag Manager -->
 </head>
 <body>
