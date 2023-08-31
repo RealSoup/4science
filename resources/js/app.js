9a5184a5a0b0a9ed1474d1ad35d53a6d5f714c66
kjk <kjk@4science.net>
2023-08-31 17:19:29 +0900

    23083109

--- resources/js/app.js
+++ resources/js/app.js
@@ -176,7 +176,7 @@ const playVue = () => {
 };
 
 import ax from '@/api/http';
-ax.get('/auth_check').then((res) => {
+ax.get('auth_check').then((res) => {
     if (res.data === 1) {
         ax.get('/api/user').then((user_res) => {
             if (user_res) {
@@ -193,7 +193,25 @@ ax.get('/auth_check').then((res) => {
     } else {
         playVue();
     }
-}).catch(() => {});
+// }).catch(() => {});
+}).catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
 
 
 
