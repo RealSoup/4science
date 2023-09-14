b640443798303c8c9e37ba4ae4876a87092c8acc
kjk <kjk@4science.net>
2023-09-14 21:25:38 +0900

    230914밤5

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -1,6 +1,11 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+        
+    <!-- 구글링 결과로 정확한 이유는 모른다. -->
+    <base href="/">
+
+    
     <meta charset="utf-8">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
     <!-- <meta name="viewport" content="width=1500, initial-scale=1"> -->
@@ -24,9 +29,7 @@
     홈페이지 메인 파일인 index 파일이 브라우저의 캐시 상태이기에 배포 바로 이전 버전의 파일이다
     여기에 새로운 js 파일을 임베드 하여 생기는 캐이 오류를 해결하기 위한 태그
     -->
-    
-    <!-- 구글링 결과로 정확한 이유는 모른다. -->
-    <base href="/">
+
     
     <!-- Google Tag Manager -->
     <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
