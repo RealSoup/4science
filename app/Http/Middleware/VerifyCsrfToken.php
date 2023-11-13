c904ba891f2e46789f7decac06c15db5527db24e
kjk <kjk@4science.net>
2023-11-14 08:33:44 +0900

    23111403

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,6 +15,7 @@ class VerifyCsrfToken extends Middleware {
     protected $except = [
         'shop/order/tossBrandPayAccessToken',
         'shop/order/tossBillingKeyIssue/*',
+        'shop/order/payReturn',        
         'shop/order/payReturn/*',
         'shop/order/payReturnMobile',
         'shop/order/payReturnPsys',
