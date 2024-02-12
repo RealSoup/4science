6162957be9182d6400487c1b780c8bb3ae7d5bd8
kjk <kjk@4science.net>
2024-02-13 08:52:29 +0900

    24021302

--- resources/views/admin/estimate/email/estimateSend.blade.php
+++ resources/views/admin/estimate/email/estimateSend.blade.php
@@ -39,8 +39,8 @@
                     </td>
                 </tr>
             </table> -->
-            <a href="https://4science.net/shop/goods/616777" target='_blank'>
-                <img src="{{ asset('storage/event/2024/0108/estimate.png') }}" />
+            <a href="https://4science.net/shop/listing/sale" target='_blank'>
+                <img src="{{ asset('storage/event/2024/0103/estimate.jpg') }}" />
             </a>
         </td>
     </tr>
@@ -179,12 +179,10 @@
     </tr>
     <tr>
         <td style='border-top:2px solid #0095eb;'>
-            <a href="https://4science.net/shop/listing/sale" target='_blank'>
-                <img src="{{ asset('storage/event/2024/0103/estimate.jpg') }}" />
-            </a>
-            <!-- <a href="https://pf.kakao.com/_AxmKUj" target='_blank'>
+            
+            <a href="https://pf.kakao.com/_AxmKUj" target='_blank'>
                 <img src="{{ asset('storage/event/2023/1205/estimate.jpg') }}" />
-            </a> -->
+            </a>
         </td>
     </tr>
     <tr>
