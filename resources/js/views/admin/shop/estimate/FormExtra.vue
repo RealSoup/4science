651397f341e8e0f00d7fb09550e5e9a04603e998
kjk <kjk@4science.net>
2024-02-14 10:22:32 +0900

    24021405

--- resources/js/views/admin/shop/estimate/FormExtra.vue
+++ resources/js/views/admin/shop/estimate/FormExtra.vue
@@ -4,13 +4,13 @@
     <b-row class="label_form">
         <b-col>
             <label>주문납품기일</label>
-            <div><b-form-input id="er_dlvy_at" v-model="value.er_dlvy_at" v-b-tooltip.hover title="입력예: 2주이내, 1개월 이내" size="sm" /></div>
-            <validation :error="$store.state.error.validations.er_dlvy_at" />
+            <div><b-form-input id="er_dlvy_at" v-model="value.er_dlvy_at" v-b-tooltip.hover title="입력예: 2주이내, 1개월 이내" size="sm"></b-form-input></div>
+            <validation :error="$store.state.error.validations.er_dlvy_at"></validation>
         </b-col>
         <b-col>
             <label>견적유효기간</label>
             <b-input-group size="sm">
-                <b-form-input v-model="value.er_effective_at" placeholder="YYYY-MM-DD" autocomplete="off" :formatter="formatDate" />
+                <b-form-input v-model="value.er_effective_at" placeholder="YYYY-MM-DD" autocomplete="off" :formatter="formatDate"></b-form-input>
                 <b-input-group-append>
                     <b-form-datepicker v-model="value.er_effective_at" size="sm" button-only right></b-form-datepicker>
                 </b-input-group-append>
@@ -19,7 +19,7 @@
 
         <b-col class="w_100">
             <label>첨부파일</label>
-            <file-upload ref="fileupload" v-model="value.file_info" :fi_group="'estimateReply'" :fi_kind="'add'" :height="124" style="margin:6px 12px; width:calc(100% - 24px);}" />
+            <file-upload ref="fileupload" v-model="value.file_info" :fi_group="'estimateReply'" :fi_kind="'add'" :height="124" style="margin:6px 12px; width:calc(100% - 24px);}"></file-upload>
             <transition name="fade">
                 <loading-modal v-if="isLoadingModalViewed" @close-modal="isLoadingModalViewed = false">첨부파일 전송중 ..... </loading-modal>
             </transition>
@@ -27,7 +27,7 @@
 
         <b-col class="w_100">
             <label>추가정보</label>
-            <div><b-form-textarea v-model="value.er_content" rows="5" size="sm" /></div>
+            <div><b-form-textarea v-model="value.er_content" rows="5" size="sm"></b-form-textarea></div>
         </b-col>
     </b-row>
 </div>
