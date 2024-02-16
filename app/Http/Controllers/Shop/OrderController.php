501268eb5100685106a9f16d68ac64b9938f82f0
kjk <kjk@4science.net>
2024-02-16 10:16:10 +0900

    24021603

--- app/Http/Controllers/Shop/OrderController.php
+++ app/Http/Controllers/Shop/OrderController.php
@@ -358,13 +358,16 @@ class OrderController extends Controller {
             //  빌링키를 활용 결제 승인 받기
             $rst_toss = self::tossCurl('tossBillingPayApprove', $obj);
         }
+
+        $rst_toss = json_decode($rst_toss);
+
         if ( auth()->user()->id == 130 ) {
             if (property_exists($rst_toss, 'message'))  //  결제 실패
                 return redirect("/shop/order/payCardFail?msg=".$rst_toss->message);
             else
                 return redirect("/shop/order/payCardFail?msg=".$rst_toss->lastTransactionKey);
         }
-        $rst_toss = json_decode($rst_toss);
+        
         self::tossPgInsert($rst_toss);
         
         //  재고 상품 구매시 수량 감소
