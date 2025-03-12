0e8093688bac8931e7e25b8d0988bb16809e000e
kjk <kjk@4science.net>
2025-03-12 15:31:09 +0900

    250301204

--- app/Http/Controllers/Admin/Shop/OrderController.php
+++ app/Http/Controllers/Admin/Shop/OrderController.php
@@ -87,6 +87,7 @@ class OrderController extends Controller {
 			$orders = $orders->whereIn('od_mng', (count($group) ? $group : ['']));
 		}
 		if (isset($data['sale_env']))	$orders = $orders->where('od_sale_env', $data['sale_env']);
+		if (isset($data['writer']))		$orders = $orders->SchWriter($data['writer']);
         if (isset($data['keyword'])){
 			$txt = $data['keyword'];
             switch ($data['mode']) {
