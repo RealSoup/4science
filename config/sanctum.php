ed736b6b4a265c8f242ce254535d366a31bae6e2
kjk <kjk@4science.net>
2025-04-01 09:55:01 +0900

    25040103

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,http://menlo.co.kr,https://menlo.co.kr,http://thz.co.kr,https://thz.co.kr',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,menlo.co.kr,thz.co.kr',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
