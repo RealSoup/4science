4e391e57228063645384bb363fe01a5a7f452f8d
kjk <kjk@4science.net>
2025-04-02 13:05:41 +0900

    25040205

--- app/Models/Shop/Goods.php
+++ app/Models/Shop/Goods.php
@@ -247,6 +247,8 @@ class Goods extends Model {
             } else {
                 if ( $gd_id > 0 ) {
                     $gd = self::with('purchaseAt')->with('maker')->find($gd_id);
+                    if( auth()->check() && auth()->user()->id == 130 ) 
+                        dd(self::with('purchaseAt')->with('maker')->find(78649));
                     foreach (GoodsModel::find(Arr::pluck($v['model'], 'gm_id')) as $gm) {  //  gm_id만 추출하여 모델 검색
                         $tmpModel = [   'type'              => 'model',
                                         'gd_id'             => $gd_id,
