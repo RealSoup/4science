84cd727d3895feef60476ea576df6dd90fe738b9
kjk <kjk@4science.net>
2025-04-10 11:54:31 +0900

    25041003

--- app/Http/Controllers/KcpController.php
+++ app/Http/Controllers/KcpController.php
@@ -185,8 +185,6 @@ class KcpController extends Controller {
                 if($age < 20)
                     $json_res["is_adult"] = 'false';
                 else {
-                    var_dump(auth()->user());
-                    dd(auth()->user()->id);
                     DB::table('users')->where('id', auth()->user()->id)->update(['adult_verified_at'=> \Carbon\Carbon::now()]);
                     $json_res["is_adult"] = 'true';
                 }
