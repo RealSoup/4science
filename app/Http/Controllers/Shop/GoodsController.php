ffb32482e947a18de7b77c65a317b725f9eb8f7d
kjk <kjk@4science.net>
2025-05-27 10:37:01 +0900

    25052703

--- app/Http/Controllers/Shop/GoodsController.php
+++ app/Http/Controllers/Shop/GoodsController.php
@@ -48,8 +48,8 @@ class GoodsController extends Controller {
 
         if ($req->filled('keyword')) {
             $referer = $_SERVER['HTTP_REFERER'] ?? null;
-            Log::info('🔍 $_SERVER[HTTP_REFERER]: ' . $referer);
-            
+            Log::info('controlloer: ' . $referer);
+
             if (if_not_my_ip($req->ip()))
                 event(new \App\Events\GoodsSearch($req->keyword, auth()->check() ? auth()->user()->id : 0, $req->ip()));  //  검색어 데이터화
             
