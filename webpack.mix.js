ac67d70d4f61008b5d0870cb59d9f3403e59f70f
kjk <kjk@4science.net>
2026-08-06 11:06:32 +0900

    26080603

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,18 +18,18 @@ mix.js('resources/js/app.js', 'public/js')
             host: "0.0.0.0",
             port: 8000,
         },
-        optimization: {
-            moduleIds: 'deterministic',
-            chunkIds: 'deterministic',
-        },
-        output: {
-            chunkFilename: 'js/chunks/[name].[contenthash:8].js',
-        },
+        // output: {
+            // path: path.resolve(__dirname, 'public'),
+            // publicPath: '/',
+            // filename: '[name].[chunkhash:8].js',  //이부분을 수정
+            
+            // chunkFilename: '[name].js?id=[chunkhash]',
+        // },
     })
     .version();
 
 
-mix.browserSync ('https://4science.net/');
+// mix.browserSync ('https://4science.net/');
 // mix.disableNotifications();
 // mix.browserSync({
 //     // proxy: 'localhost:3000',
